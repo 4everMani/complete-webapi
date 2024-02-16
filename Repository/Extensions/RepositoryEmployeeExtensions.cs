84c8dbd67e0e79f201b94e311b8058e45378505d
4evermani <jaiswal.manish49@gmail.com>
2024-02-16 11:10:47 +0530

    generic sorting implemented

--- Repository/Extensions/RepositoryEmployeeExtensions.cs
+++ Repository/Extensions/RepositoryEmployeeExtensions.cs
@@ -3,6 +3,7 @@ using System.Reflection;
 using System.Text;
 using System.Linq.Dynamic.Core;
 
+
 namespace Repository.Extensions
 {
     public static class RepositoryEmployeeExtensions
@@ -25,35 +26,13 @@ namespace Repository.Extensions
         {
             if (string.IsNullOrWhiteSpace(orderByQueryString)) return employees.OrderBy(e => e.Name);
 
-            var orderParams = orderByQueryString.Trim().Split(',');
+            var orderQuery = OrderQueryBuilder.CreateOrderQuery<Employee>(orderByQueryString);
 
-            // using reflection to get property info of Employee
-            var propertyInfos = typeof(Employee).GetProperties(BindingFlags.Public | BindingFlags.Instance);
-
-            var orderQueryBuilder = new StringBuilder();
-
-            foreach ( var param in orderParams)
-            {
-                if (string.IsNullOrWhiteSpace(param)) continue;
-
-                var propertyFromQueryName = param.Split(" ")[0];
-
-                var objectProperty = propertyInfos.FirstOrDefault(pi =>
-                pi.Name.Equals(propertyFromQueryName, StringComparison.OrdinalIgnoreCase));
-
-                if (objectProperty is null) continue;
-
-                var direction = param.EndsWith(" desc") ? "descending" : "ascending";
-
-                orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction},");
-            }
-
-            var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
-
-            if (string.IsNullOrWhiteSpace(orderQuery))
-                return employees.OrderBy(e => e.Name);
+            if (string.IsNullOrWhiteSpace(orderQuery)) return employees.OrderBy(e => e.Name);
 
             return employees.OrderBy(orderQuery);
         }
     }
+
+
 }
---
+++ Repository/Extensions/Utility/OrderQueryBuilder.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace Repository.Extensions
+{
+    public static class OrderQueryBuilder
+    {
+        public static string CreateOrderQuery<T>(string orderByQuerying)
+        {
+            var orderParams = orderByQuerying.Trim().Split(',');
+            var properyInfos = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var orderQueryBuilder = new StringBuilder();
+
+            foreach(string param in  orderParams)
+            {
+                if (string.IsNullOrWhiteSpace(param)) continue;
+
+                var propertyFromQueryName = param.Split(" ")[0];
+                var objectProperty = properyInfos.FirstOrDefault(pi => 
+                pi.Name.Equals(propertyFromQueryName, StringComparison.OrdinalIgnoreCase));
+
+                if (objectProperty is null) continue;
+
+                var direction = param.EndsWith(" desc") ? "descending" : "ascending";
+
+                orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction}, ");
+            }
+
+            var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+            return orderQuery;
+        }
+    }
+}
